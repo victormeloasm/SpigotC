f214726101b2e10005bdbfac7416d7761486a05e
victormeloasm <victormeloasm@gmail.com>
2023-06-03 08:46:50 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void spigot(int n) {
-    int len = (10 * n / 3) + 1;
+
+void spigot(int n)
+{
+
+    int ft=0,len = (10 * n / 3) + 2;  // Corrected formula
     int *digits = (int *)malloc(sizeof(int) * len);
-    if (digits == NULL) {
+    if (digits == NULL)
+    {
         printf("Memory allocation failed.\n");
         return;
     }
 
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++)
+    {
         digits[i] = 2;
     }
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         int carry = 0;
-        for (int j = len - 1; j > 0; j--) {
+        for (int j = len - 1; j > 0; j--)
+        {
             digits[j] = digits[j] * 10 + carry;
             int temp = digits[j] / (j * 2 + 1);
             digits[j] = digits[j] % (j * 2 + 1);
@@ -27,18 +33,23 @@ void spigot(int n) {
         digits[0] = digits[0] % 10;
 
         printf("%d", q);
+        if(ft==0)
+        {
+            printf(".");
+            ft++;
+        }
     }
 
     free(digits);
 }
 
-int main() {
+int main()
+{
     int n;
     printf("Enter the number of digits to generate: ");
     scanf("%d", &n);
-    printf("Pi: 3.");
+    printf("Pi: ");
     spigot(n);
     printf("\n");
-    getch();
     return 0;
 }
